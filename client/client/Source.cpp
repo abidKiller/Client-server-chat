9a17e8714cb8be967992ae571b6a1f67562edd1d
sanjidtt <sanjidtausif@gmail.com>
2019-03-25 21:11:32 +0600

    introduced packet types and packet length, however packet processing error check not handled

--- client/client/Source.cpp
+++ client/client/Source.cpp
@@ -1,20 +1,61 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
 #pragma comment(lib,"ws2_32.lib")
 #pragma warning(disable:4996)
 #include<iostream>
-#include<winSock2.h>
+#include<string>
+#include<WinSock2.h>
 
 SOCKET connection;
+
+enum Packet
+{
+	P_ChatMessage,
+	P_Test
+};
+
+bool ProcessPacket(Packet pack_type)
+{
+	switch (pack_type)
+	{
+	case P_ChatMessage:
+	{
+		int bufferlength; //length of the chat message
+		recv(connection, (char*)&bufferlength, sizeof(int), NULL); //receive buffer
+		char *buffer = new char[bufferlength + 1]; //Allocate buffer
+		buffer[bufferlength] = '\0'; //Set last character of buffer to be a null terminator so we aren't printing memory that we shouldn't be looking at
+		recv(connection, buffer, bufferlength, NULL);
+		std::cout << buffer << std::endl; //print out buffer
+		delete[] buffer; //Deallocate buffer
+		break;
+	}
+	case P_Test:
+		std::cout << "You have received the test packet!\nThis is the test packet!" << std::endl;
+		break;
+	default:
+		std::cout << "Unknown packet: " << packettype << std::endl;
+		break;
+	}
+	return true;//Does not handle if not processed
+}
+
 void ClientThread()
 {
-	char message[256]; //buffer to hold messages
+	//Handling packet types and their sizes rather than just sending char
+	Packet pack_type;
 	while (true)
 	{
-		recv(connection, message, sizeof(message), NULL); //receive buffer
-		std::cout << message << std::endl; //print buffer
+		//First get the packet type
+		recv(connection, (char*)&pack_type, sizeof(Packet), NULL); //Receive packet type from server
+
+		if (!ProcessPacket(pack_type)) //If the packet is not properly processed, however not handled now
+		{
+			break; //break out of our client handler loop
+		}
+			
 	}
 }
 
-using namespace std;
+//using namespace std;
 int main()
 {
 	WSAData ws;
@@ -26,13 +67,10 @@ int main()
 
 	}
 
-	int msgBoxReturn = MessageBoxA(NULL, "YOU ARE GOING TO CREATE A CONNECTION!!\nMight make your device vulnerable to malwares", "Attention", MB_OKCANCEL | MB_ICONASTERISK);
-
-	if (msgBoxReturn == IDOK) //creating socket
-    {
+	
 		SOCKADDR_IN addr;
 		int addrlen = sizeof(addr);
-		addr.sin_addr.s_addr = inet_addr("192.168.0.100"/*"192.168.0.11"*/ /*its an example*/);  // the ip address of lan network
+		addr.sin_addr.s_addr = inet_addr("172.17.37.17"/*"192.168.0.11"*/ /*its an example*/);  // the ip address of lan network
 		addr.sin_port = htons(1111); //port 
 		addr.sin_family = AF_INET;  //IPv4
 
@@ -43,19 +81,23 @@ int main()
 			exit(1);
 		}
 
-		cout << "CONNECTED" << endl;
+		std::cout << "CONNECTED" << endl;
 		CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)ClientThread, NULL, NULL, NULL); //Create the client thread that receives data server sends.
 
 		
-		char message[256];
+		std::string userinput; //holds the user's chat message
 		while (true)
 		{
-			std::cin.getline(message, sizeof(message)); //Get line if user presses enter and fill the buffer
-			send(connection, message, sizeof(message), NULL); //Send buffer
+			std::getline(std::cin, userinput); //Get line if user presses enter and fill the buffer
+			int bufferlength = userinput.size(); //Find buffer length
+			Packet chatmessagepacket = P_ChatMessage; //Create packet type: Chat Message to be sent to the server
+			send(connection, (char*)&chatmessagepacket, sizeof(Packet), NULL); //Send packet type: Chat Message
+			send(connection, (char*)&bufferlength, sizeof(int), NULL); //Send length of buffer
+			send(connection, userinput.c_str(), bufferlength, NULL); //Send buffer
 			Sleep(10);
 		}
 	
-	}
+	
 	system("pause");
 	return 0;
 }
--- server/server/Source.cpp
+++ server/server/Source.cpp
@@ -1,28 +1,75 @@
 //server
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
 #pragma comment(lib,"ws2_32.lib")
 #pragma warning(disable:4996)
 #include<iostream>
-#include<winSock2.h>
+#include<string>
+#include<WinSock2.h>
 
 SOCKET Connections[100];	//all connection list
 int totalconnections = 0;	//total connection count
 
-void ClientHandlerThread(int index) //index = the index in the SOCKET Connections array
+enum Packet
 {
-	char buffer[256]; //Buffer to receive and send out messages from/to the clients
-	while (true)
+	P_ChatMessage,
+	P_Test
+};
+
+bool ProcessPacket(int index, Packet pack_type)
+{
+	switch (pack_type)
 	{
-		recv(Connections[index], buffer, sizeof(buffer), NULL); //get message from client
-		for (int i = 0; i < totalconnections; i++) //For each client connection
+	case P_ChatMessage://braces used so that variables can be declared
+	{
+		int bufferlength; //Length of the string
+		//Logic for receiving a string
+
+		int recvcheck = recv(Connections[index], (char*)&bufferlength, sizeof(int), NULL); //get buffer length
+		char *buffer = new char[bufferlength]; //Allocate buffer
+		recv(Connections[index], buffer, bufferlength, NULL); //get buffer message from client
+
+		for (int i = 0; i < TotalConnections; i++) //For each client connection
 		{
-			if (i == index) //Skip user
-				continue; 
-			send(Connections[i], buffer, sizeof(buffer), NULL); //send the chat message to other client
+			if (i == index) //Don't send the chat message to the same user who sent it
+			{
+				continue; //Skip user
+			}
+				
+			Packet chatmessagepacket = P_ChatMessage; //create chat message packet to be sent
+			send(Connections[i], (char*)&chatmessagepacket, sizeof(Packet), NULL); //send chat message packet
+			send(Connections[i], (char*)&bufferlength, sizeof(int), NULL);//send the buffer length to client at index i
+			send(Connections[i], buffer, bufferlength, NULL);//send the chat message to client at index i
 		}
+
+		delete[] buffer; //Deallocate buffer to stop from leaking memory
+		break;
 	}
+	default:
+		std::cout << "Unknown packet received." << std::endl;
+		break;
+	}
+	return true;//did not handle the proceess for not processing packet
 }
 
-using namespace std;
+void ClientHandlerThread(int index) //index = the index in the SOCKET Connections array
+{
+	while (true)
+	{
+		//Getting Packet type
+		Packet pack_type;
+		recv(Connections[index], (char*)&pack_type, sizeof(Packet), NULL); //Receive packet type from client
+		
+		//Packet processing check is not implemented yet so it would not work
+		if (!ProcessPacket(index, pack_type)) //If the packet is not properly processed
+		{
+			break; //break out of our client handler loop
+		}
+			
+	}
+	closesocket(Connections[index]);
+}
+
+//using namespace std;
 int main()
 {
 	WSAData ws;
@@ -35,11 +82,10 @@ int main()
 
 	int msgBoxReturn = MessageBoxA(NULL, "YOU ARE GOING TO CREATE A CONNECTION!!\nMight make your device vulnerable to malwares", "Attention", MB_OKCANCEL | MB_ICONASTERISK);
 
-	if (msgBoxReturn == IDOK) //creating socket
-	{ 
+	
 		SOCKADDR_IN addr;
 		int addrlen = sizeof(addr);
-		addr.sin_addr.s_addr = inet_addr(/*"127.0.0.1"*/"192.168.0.100" /*its an example*/);  // the ip address of lan network
+		addr.sin_addr.s_addr = inet_addr(/*"127.0.0.1"*/"172.17.37.17" /*its an example*/);  // the ip address of lan network
 		addr.sin_port = htons(1111); //port 
 		addr.sin_family = AF_INET;  //IPv4
 
@@ -51,24 +97,36 @@ int main()
 
 		//socket to hold clients connection
 		SOCKET newConn;
-		for (int i = 0; i < 10; i++) {
+		int conn_count = 0; 
+		for (int i = 0; i < 10; i++)
+		{
 			newConn = accept(slisten, (SOCKADDR*)&addr, &addrlen);
 
 			if (newConn == 0)
 			{
-				MessageBoxA(NULL, "Failed to accept client's connection ", "ERROR", MB_OK);
+				std::cout << "Failed to accept the client's connection." << std::endl;
 			}
 			else {
-				cout << "client connected!!" << endl;
-				char msg[256] = "WELLCOME";
-				send(newConn, msg, sizeof(msg), NULL);
+				std::cout << "client connected!!" << endl;
+	
 				Connections[i] = newConn;
 				totalconnections++;
 				CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)ClientHandlerThread, (LPVOID)(i), NULL, NULL); //To Create Thread to handle this client.
+				
+				std::string buff_test = "Message: Welcome! This is the message of the day!.";
+				int size = buff_test.size(); //Get size of message in bytes and store it in int size
+				Packet chatmessagepacket = P_ChatMessage; //Create packet type: Chat Message to be sent to the server
+				send(Connections[i], (char*)&chatmessagepacket, sizeof(Packet), NULL); //Send packet type: Chat Message
+				send(Connections[i], (char*)&size, sizeof(int), NULL); //send Size of message
+				send(Connections[i], buff_test.c_str(), buff_test.size(), NULL); //send Message
+
+				Packet testpacket = P_Test;
+				send(Connections[i], (char*)&testpacket, sizeof(Packet), NULL); //Send test packet
+				
 			}
 		}
 		system("pause");
-	}
+	
 	
 	exit(1);
 }
