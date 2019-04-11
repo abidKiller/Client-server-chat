8cb5ce3b719de74affd04533ef4c93a70878b4bb
tauhidtamim <46972380+tauhidtamim@users.noreply.github.com>
2019-04-11 13:54:55 +0600

    introducing image file sharing

--- client/client/Source.cpp
+++ client/client/Source.cpp
@@ -1,3 +1,4 @@
+#include "pch.h"
 //client
 #include "Client.h"
 
@@ -7,15 +8,16 @@ int main()
 
 	if (!myClient.ConnectServer()) //If client fails to connect...
 	{
-		std::cout << "Failed to connect to server..." << std::endl;
+		cout << "Failed to connect to server..." << endl;
 		system("pause");
 		return 1;
 	}
 
+	myClient.RequestFile("b6-2.jpg");
 	std::string userinput; //holds the user's chat message
 	while (true)
 	{
-		std::getline(std::cin, userinput); //Get line if user presses enter and fill the buffer
+		getline(cin, userinput); //Get line if user presses enter and fill the buffer
 		if (!myClient.SendString(userinput)) {		//send string to server
 			break;		//if send string failed	leave loop as server connection lost	//
 		}
